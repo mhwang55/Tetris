aa196504dcd46767b717ba7e14372ee1fc9fd8ba
Max <mwang@rexchange.com>
2018-11-19 02:40:13 -0800

    tetris

---
+++ Assets/scripts/EventManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnGUI()
+	{
+		/*
+		for (int i = 0; i < 5; i++)
+		{
+			Falling falling = GetComponent<Falling>();
+		}
+		//*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
